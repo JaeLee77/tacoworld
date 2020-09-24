6be5490af03032ba9ebc22eed4b66b1deb62a563
Jae <jyl71@georgetown.edu>
2020-09-24 20:40:52 +0000

    tacoworld: v cool good for you eamon but I wanted tacos first bubs so back off

--- tacoworld.py
+++ tacoworld.py
@@ -1,3 +1,4 @@
 print("I, Zach Lamberty, want tacos!")
 print("I, Eamon Lamberty, want tacos!")
-
+print("I, Zach Lamberty, want tacos more than *anyone*, especially more than Eamon!")
+print("I, Eamon Lamberty, want tacos!")
